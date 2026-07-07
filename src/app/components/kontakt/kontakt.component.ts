2570811e1f462cc95a979fafbca0fdca892d563b
Ch1charito <r.buschmann.work@gmail.com>
2026-07-07 17:10:05 +0200

    added php, form and functions for contact section

--- src/app/components/kontakt/kontakt.component.ts
+++ src/app/components/kontakt/kontakt.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-kontakt',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './kontakt.component.html',
   styleUrl: './kontakt.component.scss'
 })
 export class KontaktComponent {
 
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  mailTest = true;      // change to false when on webserver true only for local testing
+
+  post = {
+    endPoint: 'https://DEINE-DOMAIN.de/sendMail.php', // change domain later!!!
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  sendSuccess = false;
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: () => {
+            ngForm.resetForm();
+            this.sendSuccess = true;
+            setTimeout(() => this.sendSuccess = false, 3000);
+          },
+          error: (error) => console.error(error),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+      this.sendSuccess = true;
+      setTimeout(() => this.sendSuccess = false, 3000);
+    }
+  }
 }
