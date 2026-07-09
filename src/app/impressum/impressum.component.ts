01d090fe64900ef36a4860f16b2ebebe54c46a89
Ch1charito <r.buschmann.work@gmail.com>
2026-07-09 11:42:09 +0200

    feat: add legal pages with basic i18n support

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-impressum',
-  imports: [NavbarComponent, FooterComponent, RouterLink],
+  imports: [NavbarComponent, FooterComponent, RouterLink, TranslatePipe],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
